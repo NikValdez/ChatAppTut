9999f1e08c183668467128452a7c93791b5e2ff3
NikValdez <nerdvestor@gmail.com>
2021-03-04 18:21:27 -0800

    Fix issues

--- web/src/App.js
+++ web/src/App.js
@@ -1,71 +1,72 @@
-import React, { useState, useEffect } from 'react'
-import io from 'socket.io-client'
-import TextField from '@material-ui/core/TextField'
-import './App.css'
-
-const socket = io.connect('http://localhost:4000')
+import TextField from "@material-ui/core/TextField"
+import React, { useEffect, useRef, useState } from "react"
+import io from "socket.io-client"
+import "./App.css"
 
 function App() {
-  const [state, setStaet] = useState({ message: '', name: '' })
-  const [chat, setChat] = useState([])
-  
-   useEffect(() => {
-    socket.on("message", ({ name, message }) => {
-      setChat([...chat, { name, message }])
-    })
-  }, [state])
+	const [ state, setState ] = useState({ message: "", name: "" })
+	const [ chat, setChat ] = useState([])
 
-  const onTextChange = e => {
-    setStaet({ ...state, [e.target.name]: e.target.value })
-  }
+	const socketRef = useRef()
 
-  const onMessageSubmit = e => {
-    e.preventDefault()
-    const { name, message } = state
-    socket.emit('message', { name, message })
-    setStaet({ message: '', name })
-  }
+	useEffect(
+		() => {
+			socketRef.current = io.connect("http://localhost:4000")
+			socketRef.current.on("message", ({ name, message }) => {
+				setChat([ ...chat, { name, message } ])
+			})
+			return () => socketRef.current.disconnect()
+		},
+		[ chat ]
+	)
 
-  const renderChat = () => {
-    return chat.map(({ name, message }, index) => (
-      <div key={index}>
-        <h3>
-          {name}: <span>{message}</span>
-        </h3>
-      </div>
-    ))
-  }
+	const onTextChange = (e) => {
+		setState({ ...state, [e.target.name]: e.target.value })
+	}
 
-  return (
-    <div className="card">
-      <form onSubmit={onMessageSubmit}>
-        <h1>Messanger</h1>
-        <div className="name-field">
-          <TextField
-            name="name"
-            onChange={e => onTextChange(e)}
-            value={state.name}
-            label="Name"
-          />
-        </div>
-        <div>
-          <TextField
-            name="message"
-            onChange={e => onTextChange(e)}
-            value={state.message}
-            id="outlined-multiline-static"
-            variant="outlined"
-            label="Message"
-          />
-        </div>
-        <button>Send Message</button>
-      </form>
-      <div className="render-chat">
-        <h1>Chat Log</h1>
-        {renderChat()}
-      </div>
-    </div>
-  )
+	const onMessageSubmit = (e) => {
+		const { name, message } = state
+		socketRef.current.emit("message", { name, message })
+		e.preventDefault()
+		setState({ message: "", name })
+	}
+
+	const renderChat = () => {
+		return chat.map(({ name, message }, index) => (
+			<div key={index}>
+				<h3>
+					{name}: <span>{message}</span>
+				</h3>
+			</div>
+		))
+	}
+
+	return (
+		<div className="card">
+			<form onSubmit={onMessageSubmit}>
+				<h1>Messanger</h1>
+				<div className="name-field">
+					<TextField name="name" onChange={(e) => onTextChange(e)} value={state.name} label="Name" />
+				</div>
+				<div>
+					<TextField
+						name="message"
+						onChange={(e) => onTextChange(e)}
+						value={state.message}
+						id="outlined-multiline-static"
+						variant="outlined"
+						label="Message"
+					/>
+				</div>
+				<button>Send Message</button>
+			</form>
+			<div className="render-chat">
+				<h1>Chat Log</h1>
+				{renderChat()}
+			</div>
+		</div>
+	)
 }
 
 export default App
+
